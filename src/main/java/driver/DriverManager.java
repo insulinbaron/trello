11749b04c9f4906cd95b6a02af6c6c46ab318b12
sobolevvn <vsobolev@lanit.ru>
2023-05-11 15:48:42 +0300

    update

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -36,6 +36,7 @@ public class DriverManager {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920x1080");
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--headless");
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
